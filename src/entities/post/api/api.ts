6292a52cae04f3ce1c4b37def010cbaabd7b72f9
LEEYEONSEONG <dustjd1535@gmail.com>
2025-04-30 23:53:34 +0900

    Fix : add post default option

--- src/entities/post/api/api.ts
+++ src/entities/post/api/api.ts
@@ -42,11 +42,17 @@ export async function fetchTags(): Promise<Tag[]> {
 }
 
 export async function addPost(post: Omit<Post, "id">): Promise<Post> {
+  const payload = {
+    ...post,
+    userId: 1,
+  }
+
   const res = await fetch("/api/posts/add", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(post),
+    body: JSON.stringify(payload),
   })
+
   return await res.json()
 }
 
