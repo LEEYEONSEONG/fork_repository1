14ddc2d0efed33bd17e109d9ea9844f0ad9f330a
LEEYEONSEONG <dustjd1535@gmail.com>
2025-04-30 21:32:30 +0900

    Add : queryClient provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,16 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import { BrowserRouter as Router } from "react-router-dom"
+import { QueryClientProvider } from "@tanstack/react-query"
+import { queryClient } from "./shared/api/queryClient"
 import App from "./App"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <App />
+      </Router>
+    </QueryClientProvider>
   </React.StrictMode>,
 )
